5e617e1c35c106193601a71b6b40efac18b336dc
ari.ikalainen <ari.ikalainen@AUSB-TEMP2>
2024-02-06 13:07:57 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BastaLunchApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
